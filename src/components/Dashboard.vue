3c75e5d9a2effe50c7d15541b9cc999f27b12d56
Edmond <edipetres@gmail.com>
2017-10-01 18:03:45 +0200

    close #3 display two station boards

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="hello">
-    <iframe id="kl-station" title="Example2" width="700" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.rejseplanen.dk/bin/help.exe/mn?L=vs_tus.vs_new&tpl=monitor&station=10120&view=dep&stopFrequency=high&p1=bus&p1rows=15&preview=200&p1icon=yes&p1freeTitle=Departures%20from%20Klampenborgvej%20St."></iframe>
-    <iframe id="kl-station" title="Example2" width="700" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.rejseplanen.dk/bin/help.exe/mn?L=vs_tus.vs_new&tpl=monitor&station=8600675&view=dep&stopFrequency=high&p1=stog&p1rows=15&preview=200&p1icon=yes&p1freeTitle=Departures%20from%20Lyngby%20St."></iframe>
-
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 no-gutters">
+        <iframe class="stboard" height="1080" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.rejseplanen.dk/bin/help.exe/mn?L=vs_tus.vs_new&tpl=monitor&station=10120&view=dep&stopFrequency=high&p1=bus&p1rows=15&preview=200&p1icon=yes&p1freeTitle=Klampenborgvej%20St."></iframe>
+      </div>
+      <div class="col-md-6 no-gutters">
+        <iframe class="stboard" height="1080" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.rejseplanen.dk/bin/help.exe/mn?L=vs_tus.vs_new&tpl=monitor&station=8600675&view=dep&stopFrequency=high&p1=stog&p1rows=15&preview=200&p1icon=yes&p1freeTitle=Lyngby%20St."></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,26 +24,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#kl-station{
+
+.no-gutters {
+  margin-right: 0;
+  margin-left: 0;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.stboard {
+  width: 100%;
+}
+
+/* scale content size down */
+#wrap {
+  height: 100vh;
+  padding: 0;
+  overflow: hidden;
+}
+
+#frame {
   height: 100vh;
 }
 
-h1,
-h2 {
-  font-weight: normal;
+#frame {
+  -ms-zoom: 0.75;
+  -moz-transform: scale(0.75);
+  -moz-transform-origin: 0 0;
+  -o-transform: scale(0.75);
+  -o-transform-origin: 0 0;
+  -webkit-transform: scale(0.75);
+  -webkit-transform-origin: 0 0;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
